a2ce7713dfb28102f2918d2cc0faf4dcf1900647
Saad <93708296+Cyber-Dioxide@users.noreply.github.com>
2022-08-31 22:11:14 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 # shellcheck disable=SC2028
-echo "\n[*] This may take a while \n[+] Installing required packages ..."
+echo "[*] This may take a while"
+echo "[+] Installing required packages ..."
 sudo apt install python3 > /dev/null
 echo "[+] Successfully installed python3"
-sudo apt install python3-pip > dev/null
+sudo apt install python3-pip > /dev/null
 echo "[+] Successfully installed python3-pip"
 pip install -r requirements.txt > /dev/null
 echo "[+] Successfully installed python3 libraries"
